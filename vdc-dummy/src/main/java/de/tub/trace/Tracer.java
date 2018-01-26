e3759e88c286d2283be445cac70d165f047cf41a
Sebastian Werner <werner@tu-berlin.de>
2018-01-26 11:55:00 +0100

    added java tracing module

--- vdc-dummy/src/main/java/de/tub/trace/Tracer.java
+++ vdc-dummy/src/main/java/de/tub/trace/Tracer.java
@@ -3,14 +3,17 @@ package de.tub.trace;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import okhttp3.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
-
 @Component
 public class Tracer implements Callback{
+
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
 
     private final OkHttpClient client;
     private final String uri;
@@ -24,6 +27,7 @@ public class Tracer implements Callback{
                 .build();
         mapper = new ObjectMapper();
         this.uri = uri;
+        log.info("started vdc java tracer @%s",uri);
     }
 
     public void push(Trace trace) {
@@ -33,6 +37,7 @@ public class Tracer implements Callback{
             builder.put(RequestBody.create(MediaType.parse("application/json"), mapper.writeValueAsString(trace)));
             Request request = builder.build();
             client.newCall(request).enqueue(this);
+            log.debug("send trace request for %s",trace.getSpanId());
         } catch (JsonProcessingException e) {
         }
     }
@@ -46,6 +51,7 @@ public class Tracer implements Callback{
 
                 Request request = builder.build();
                 client.newCall(request).enqueue(this);
+                log.debug("send trace close request for %s",trace.getSpanId());
             } catch (JsonProcessingException e) {
                 e.printStackTrace();
             }
@@ -54,11 +60,11 @@ public class Tracer implements Callback{
 
     @Override
     public void onFailure(Call call, IOException e) {
-        e.printStackTrace();
+        log.debug("failed to send trace",e);
     }
 
     @Override
     public void onResponse(Call call, Response response) throws IOException {
-
+        log.debug("trace send %d",response.code());
     }
 }
