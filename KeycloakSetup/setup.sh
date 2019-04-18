cde92f8f5f54b23f57c363d2fecac431b6b17efd
Sebastian <werner@tu-berlin.de>
2019-04-18 14:23:03 +0200

    fixed some configuration issues

--- KeycloakSetup/setup.sh
+++ KeycloakSetup/setup.sh
@@ -1,2 +1,15 @@
-#! /bin/sh
-./kcc-linux-amd64 --headless --address keycloak:8000 --bl blueprint.json --cf config.json
+#/bin/sh
+COMPONENT_URL="http://keycloak:8000/v1/routes"
+attempts=0
+until $(curl --output /dev/null --silent $COMPONENT_URL); do
+    if [ ${attempts} -eq 100 ]; then
+      echo "Max attempts reached, cannot connect to component"
+      exit 1
+    fi
+
+    printf '.'
+    attempts=$(($attempts+1))
+    sleep 9
+done
+
+./kcc --headless --address http://keycloak:8000 --bl blueprint.json --cf users.json
